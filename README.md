fc8ac82eeea2f6d555e187b73bc222057787c286
pielun-nti <pierre.lundstrom@elev.ga.ntig.se>
2020-10-16 10:59:31 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# noderep
-# soloadventure
 # soloadventure
--- soloadventure.sql
+++ soloadventure.sql
@@ -29,7 +29,7 @@ CREATE TABLE `links` (
   `story_id` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `story_id` (`story_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `links` (
 
 LOCK TABLES `links` WRITE;
 /*!40000 ALTER TABLE `links` DISABLE KEYS */;
-INSERT INTO `links` VALUES (1,'The description',1,1),(2,'desc',2,2),(3,NULL,3,3),(4,NULL,4,4),(5,NULL,5,5),(6,NULL,6,6),(7,NULL,7,7),(8,NULL,8,8);
+INSERT INTO `links` VALUES (1,'Jag ska vara försiktig',2,1),(2,'Sluta vara så orolig kärring',3,1),(3,'Tack, jag mår bra.',4,2),(4,'Det angår inte dig!',5,2),(5,'Okej.',4,2),(6,'Tack, jag mår bra.',4,3),(7,'Det angår inte dig!',5,3),(8,'Okej.',4,3),(9,'Till min mormor.',6,4),(10,'Vart jag ska har du inte med att göra',6,4),(11,'Jag vill inte berätta för dig',6,4),(12,'Till min mormor.',6,5),(13,'Vart jag ska har du inte med att göra',6,5),(14,'Jag vill inte berätta för dig',6,5),(15,'Kaka och vin. Igår var det bak-dag och min stackars sjuka mormor skall få något gott så att hon blir bättre.',7,6),(16,'Ingenting.',7,6),(17,'Ingenting. Gå nu annars skjuter jag dig nästa gång jag ser dig.',8,6),(18,'Hon bor en bra bit in i skogen. Hennes hus står under tre stora ekträd. Du känner säkert till det.',9,7),(19,'Varför undrar du?',9,7),(20,'Mamma har sagt att jag inte ska prata med främlingar',10,7),(21,'Hon bor en bra bit in i skogen. Hennes hus står under tre stora ekträd. Du känner säkert till det.',9,8),(22,'Varför undrar du?',9,8),(23,'Mamma har sagt att jag inte ska prata med främlingar',10,8),(24,'Ja, det skulle säkert göra mormor glad.',11,9),(25,'Nej jag vill inte göra det.',12,9),(26,'Glöm det din dumskalle.',13,9),(27,'Ja, det skulle säkert göra mormor glad.',11,10),(28,'Nej jag vill inte göra det.',12,10),(29,'Glöm det din dumskalle.',13,10);
 /*!40000 ALTER TABLE `links` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,33 +70,6 @@ INSERT INTO `meeps` VALUES (1,0,'Hello world!','2020-09-25 08:57:33','2020-09-25
 UNLOCK TABLES;
 
 --
--- Table structure for table `stories`
---
-
-DROP TABLE IF EXISTS `stories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `stories` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `body` text,
-  `alt1` text,
-  `alt2` text,
-  `alt1id` bigint unsigned NOT NULL,
-  `alt2id` bigint unsigned NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `stories`
---
-
-LOCK TABLES `stories` WRITE;
-/*!40000 ALTER TABLE `stories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `story`
 --
 
@@ -106,12 +79,8 @@ DROP TABLE IF EXISTS `story`;
 CREATE TABLE `story` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `body` text,
-  `alt1` text,
-  `alt2` text,
-  `alt2id` int unsigned NOT NULL,
-  `alt1id` int unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -120,7 +89,7 @@ CREATE TABLE `story` (
 
 LOCK TABLES `story` WRITE;
 /*!40000 ALTER TABLE `story` DISABLE KEYS */;
-INSERT INTO `story` VALUES (1,'En gång för länge sedan fanns det en liten flicka, som alla tyckte mycket om, men mest av alla hennes mormor. Hon skulle kunna ge detta barn vad som helst. En gång gav hon henne en liten röd luva, som passade henne så bra att hon aldrig ville ha på sig något annat. Hon kallades därför för Rödluvan.\r\n\r\nEn dag sa hennes mor till henne ”Kom Rödluvan, här är en bit av den nybakade kakan och en flaska vin. Tag med dem till din mormor, hon är lite sjuk och det skulle göra henne gott. Gå innan det blir för varmt. Gå försiktigt och lämna inte vägen, för då kan du ramla och slå sönder flaskan. När du kommer in i hennes rum, skall du inte glömma att säga god morgon, innan du springer runt i varje hörn.”\r\n\r\nVad ska Rödluvan svara?','Jag ska vara försiktig','Sluta vara så orolig kärring',3,2),(2,'Rödluvan gav sig iväg.\r\nMormor bodde ute i skogen långt från byn där Rödluvan bodde. Just när Rödluvan gick in i skogen mötte en varg henne. Rödluvan visste inte vad det var för en konstig figur och blev inte alls rädd för honom.\r\n\r\n”God dag, lilla Rödluvan, hur mår du min lilla vän”, sa han.\r\n\r\nVad vill du att Rödluvan ska svara vargen?','Tack, jag mår bra.','Det angår inte dig!',5,4),(3,'Rödluvans mamma blev arg och gick därifrån.\r\nMormor bodde ute i skogen långt från byn där Rödluvan bodde. Just när Rödluvan gick in i skogen mötte en varg henne. Rödluvan visste inte vad det var för en konstig figur och blev inte alls rädd för honom.\r\n\r\n”God dag, lilla Rödluvan, hur mår du min lilla vän”, sa han.\r\n\r\nVad vill du att Rödluvan ska svara vargen?','Tack, jag mår bra.','Det angår inte dig!',5,4),(4,'\"Okej\" sa vargen. \"Vart skall du ta vägen så tidigt?” frågade vargen nyfikt. Vad vill du att Rödluvan ska svara vargen?','Till min mormor.','Skulle inte du vilja veta',5,5),(5,'Vargen fnös och visade framtänderna. \"Vart skall du ta vägen så tidigt?” frågade vargen surt. Vad vill du att Rödluvan ska svara vargen?','Till min mormor','Skulle inte du vilja veta',7,6),(6,'\"Så trevligt\" sa vargen. \"Vad har du i korgen?”, frågade vargen. Vad vill du att Rödluvan ska svara vargen?','Kaka och vin. Igår var det bak-dag och min stackars sjuka mormor skall få något gott så att hon blir bättre.','Jag har med mig en bazooka som jag ska skjuta ihjäl dig med om jag ser dig igen.',8,7),(8,' \"Var bor din mormor?”, frågade vargen.','\"Hon bor en bra bit in i skogen. Hennes hus står under tre stora ekträd. Du känner säkert till det”, svarade Rödluvan.\r\n\r\n','Be om ursäkt till vargen',0,0),(9,'Vargens ansiktsuttryck blev rödare och Rödluvan såg på honom att vargen höll på att bli rasande.  \"Var bor din mormor?”, frågade vargen.','\"Hon bor en bra bit in i skogen. Hennes hus står under tre stora ekträd. Du känner säkert till det”, svarade Rödluvan.\r\n\r\n','Be om ursäkt till vargen',0,0);
+INSERT INTO `story` VALUES (1,'En gång för länge sedan fanns det en liten flicka, som alla tyckte mycket om, men mest av alla hennes mormor. Hon skulle kunna ge detta barn vad som helst. En gång gav hon henne en liten röd luva, som passade henne så bra att hon aldrig ville ha på sig något annat. Hon kallades därför för Rödluvan.\r\n\r\nEn dag sa hennes mor till henne ”Kom Rödluvan, här är en bit av den nybakade kakan och en flaska vin. Tag med dem till din mormor, hon är lite sjuk och det skulle göra henne gott. Gå innan det blir för varmt. Gå försiktigt och lämna inte vägen, för då kan du ramla och slå sönder flaskan. När du kommer in i hennes rum, skall du inte glömma att säga god morgon, innan du springer runt i varje hörn.”\r\n\r\nVad ska Rödluvan svara?'),(2,'Rödluvan gav sig iväg.\r\nMormor bodde ute i skogen långt från byn där Rödluvan bodde. Just när Rödluvan gick in i skogen mötte en varg henne. Rödluvan visste inte vad det var för en konstig figur och blev inte alls rädd för honom.\r\n\r\n”God dag, lilla Rödluvan, hur mår du min lilla vän”, sa han.\r\n\r\nVad vill du att Rödluvan ska svara vargen?'),(3,'Rödluvans mamma blev arg och gick därifrån.\r\nMormor bodde ute i skogen långt från byn där Rödluvan bodde. Just när Rödluvan gick in i skogen mötte en varg henne. Rödluvan visste inte vad det var för en konstig figur och blev inte alls rädd för honom.\r\n\r\n”God dag, lilla Rödluvan, hur mår du min lilla vän”, sa han.\r\n\r\nVad vill du att Rödluvan ska svara vargen?'),(4,'\"Okej\" sa vargen. \"Vart skall du ta vägen så tidigt?” frågade vargen nyfikt. Vad vill du att Rödluvan ska svara vargen?'),(5,'Vargen fnös och visade framtänderna. \"Vart skall du ta vägen så tidigt?” frågade vargen surt. Vad vill du att Rödluvan ska svara vargen?'),(6,'\"Så trevligt\" sa vargen. \"Vad har du i korgen?”, frågade vargen. Vad vill du att Rödluvan ska svara vargen?'),(7,' \"Var bor din mormor?”, frågade vargen.'),(8,'Vargens ansiktsuttryck blev rödare och Rödluvan såg på honom att vargen höll på att bli rasande.  \"Var bor din mormor?”, frågade vargen.'),(9,'”Vilken späd liten varelse”, tänkte vargen. ”Vilken fin, fyllig munsbit hon kommer att bli. Hon kommer att bli mycket godare än den gamla kvinnan. Jag måste vara listig så att jag kan fånga båda två.” Så vargen promenerade en stund vid Rödluvans sida och sa sedan: ”Se så vackra blommor det växer här runt omkring. Skall du inte se dig lite omkring, lyssna på fåglarna och plocka en bukett blommor till din mormor?'),(10,'Kommer sen'),(11,'Kommer sen'),(12,'Kommer sen'),(13,'Kommer sen');
 /*!40000 ALTER TABLE `story` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -162,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-15  9:35:41
+-- Dump completed on 2020-10-16 10:59:16
