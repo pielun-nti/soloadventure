e5e90a760cbdf09a35c3b4801eaaa6d7cfbdf5a5
pielun-nti <pierre.lundstrom@elev.ga.ntig.se>
2020-10-29 12:08:55 +0100

    removed some unnecessary files

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var storyRouter = require('./routes/story')
 
 var app = express();
@@ -31,7 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/story', storyRouter)
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
