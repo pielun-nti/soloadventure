cdaaf1cafddd45a631edd1dd20ff13492677fd7b
pielun-nti <pierre.lundstrom@elev.ga.ntig.se>
2020-10-29 12:01:18 +0100

    moved db file

--- routes/story.js
+++ routes/story.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { query } = require('../config/db');
+const { query } = require('../models/db');
 
 router.get('/', function (req,res,next){
   res.redirect('/story/1')
